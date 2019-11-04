de47553bb60524840b9d7b81392736903f0c0b3f
hermann.jirka <hermann.jirka@gmail.com>
2019-11-05 00:15:39 +0100

    new react project

--- src/App.js
+++ src/App.js
@@ -1,26 +1,46 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
+import Person from './Person/Person';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+
+  state = {
+    persons: [
+      { name: 'Max', age: 23, company: 'CocaCola' },
+      { name: 'George', age: 36, company: 'Oracle' },
+      { name: 'Karl', age: 42, company: 'Linux' }
+    ],
+    otherState: 'Some kind of state'
+  }
+
+  switchName = () => {
+    console.log('Was clicked!');
+    this.setState(
+      {
+        persons:
+          [{ name: 'Maxamiliano', age: 29, company: 'CocaCola' },
+          { name: 'George', age: 36, company: 'Oracle' },
+          { name: 'Karl', age: 34, company: 'Microsoft' }]
+      }
+    )
+  }
+
+  render() {
+    return (
+      <div className="App" >
+        <h1>I am bobo</h1>
+        <p>Working!!!</p>
+        <button onClick={this.switchName}>Switch!</button>
+        {/* <Person name="Max" age="23" />
+        <Person name="Pepa" age="32" />
+        <Person name="Jarda" age="56" />
+        <Person name="Jirka" age="34" />
+        <Person name="Max" age="12">My favourite hobbie: Racing</Person> */}
+        <Person name={this.state.persons[0].name} age={this.state.persons[0].age} company={this.state.persons[0].company} />
+        <Person name={this.state.persons[1].name} age={this.state.persons[1].age} company={this.state.persons[1].company} />
+        <Person name={this.state.persons[2].name} age={this.state.persons[2].age} company={this.state.persons[2].company} /></div>
+    );
+  }
 }
 
 export default App;
